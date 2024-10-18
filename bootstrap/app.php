91bf1c03219a2004b2a238742f4550623e8e6ce9
José Thiago <jthiagopereira@gmail.com>
2024-10-18 10:08:09 -0300

    correcao de login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,7 +31,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'tenant.filesystems' => TenantFilesystems::class,
             'ensureUserHasAccess' => \App\Http\Middleware\EnsureUserHasAccess::class,
-            'CheckSessionExpiration' => \App\Http\Middleware\CheckSessionExpiration::class,
+            //'CheckSessionExpiration' => \App\Http\Middleware\CheckSessionExpiration::class,
 
 
         ]);
