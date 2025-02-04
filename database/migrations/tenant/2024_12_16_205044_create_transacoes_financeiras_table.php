f13659549d9774c640fc5bddaeac69d128fc8866
José Thiago <jthiagopereira@gmail.com>
2025-02-04 13:48:30 -0300

    correcao de tipo de variavel lancamento_padrao_id 6parte

--- database/migrations/tenant/2024_12_16_205044_create_transacoes_financeiras_table.php
+++ database/migrations/tenant/2024_12_16_205044_create_transacoes_financeiras_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->enum('tipo', ['entrada', 'saida']);
             $table->decimal('valor', 15, 2);
             $table->string('descricao', 255)->nullable();
-            $table->foreignId('lancamento_padrao_id')->nullable()->constrained('lancamento_padraos')->onDelete('set null');
+            $table->foreignId('lancamento_padrao_id')->constrained('lancamento_padraos');
             $table->unsignedBigInteger('movimentacao_id')->nullable(); // Relaciona com `movimentacoes`
             $table->unsignedBigInteger('centro')->nullable();
             $table->string('tipo_documento', 50)->nullable();
