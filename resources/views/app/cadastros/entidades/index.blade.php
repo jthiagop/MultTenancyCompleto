5e964c6f2dc9f9a06bf9896a23c34567215512fc
José Thiago <jthiagopereira@gmail.com>
2026-02-23 14:56:27 -0300

    refactor: trocar ícone de busca por bi-calendar-week na listagem de entidades

--- resources/views/app/cadastros/entidades/index.blade.php
+++ resources/views/app/cadastros/entidades/index.blade.php
@@ -23,10 +23,7 @@
                                     <div class="card-title">
                                         <!--begin::Search-->
                                         <div class="d-flex align-items-center position-relative my-1">
-                                            <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-4">
-                                                <span class="path1"></span>
-                                                <span class="path2"></span>
-                                            </i>
+                                            <i class="bi bi-calendar-week fs-3 position-absolute ms-4"></i>
                                             <input type="text" id="kt_entidades_search"
                                                 class="form-control form-control-solid w-250px ps-12"
                                                 placeholder="Buscar entidade..." />
